88638a24afb1e62f196bf4d25a63a53ed71d9602
unlimited1974@aol.com <unlimited1974@aol.com>
2022-01-09 16:45:59 -0600

    commit tree 13f746331b67348b4906ecb03a63eec901dc694a

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+
+const random = 2;
+Math.random(random > 0);
+
+const num3 = 16;
+const num4 = 4;
+const mod = num3 / num4;
+
+const max = 20;
+Math.max(1, 2, 3, 20);
